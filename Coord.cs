ca3a67f1c45a03135a66d1e4cb54a16a103329fd
Daniil <Soboliev.danik@gmail.com>
2023-03-11 11:32:26 +0200

    Create struct for coords

---
+++ Coord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public struct Coord
+{
+    public int x;
+    public int y;
+
+    public Coord(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
