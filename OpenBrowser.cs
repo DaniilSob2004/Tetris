09f9976cc7a3a7194b60e51f6c2b42ab140ee165
Daniil <Soboliev.danik@gmail.com>
2023-03-28 19:46:10 +0300

    Create class OpenBrowser for open url in browser

---
+++ OpenBrowser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+public class OpenBrowser
+{
+    public static void OpenGitHub()
+    {
+        // открываем ссылку на мой гитхаб в браузере 
+        string url = "https://github.com/DaniilSob2004";
+        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+    }
+}
