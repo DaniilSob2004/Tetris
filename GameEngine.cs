ad13243967fd11560c7438b1367c215a47d057dc
Daniil <Soboliev.danik@gmail.com>
2023-04-20 12:57:22 +0300

    Create GameEngine for keypress event

---
+++ GameEngine.cs
@@ -0,0 +1,26 @@
+﻿namespace Tetris
+{
+    // Класс движок, для вызова события при нажатии клавиши
+    public class GameEngine
+    {
+        public delegate void DelKeyPressed(ConsoleKeyInfo keyInfo);
+        public event DelKeyPressed KeyPressed;
+
+        // добавляем обработчик к событию
+        public GameEngine(DelKeyPressed delKeyPressed) => KeyPressed += delKeyPressed;
+
+        public void Start()
+        {
+            while (true)
+            {
+                // если былы нажата клавиша и какой то обработчик есть
+                if (Console.KeyAvailable && KeyPressed != null)
+                {
+                    ConsoleKeyInfo k = Console.ReadKey(true);
+                    KeyPressed(k);
+                }
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
